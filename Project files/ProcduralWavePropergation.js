bc8998cc1a8f8af5386a978d9bc8111b08d7e86b
shardros <shardro.s@gmail.com>
2018-10-14 16:02:49 +0100

    Refactored to be nice

--- Project files/ProcduralWavePropergation.js
+++ Project files/ProcduralWavePropergation.js
@@ -1,64 +1,87 @@
+var xlen = 250;                         //Define grid size
+var ylen = 250;
+
+var startCord = {x: 1, y: 1};          //Create cord-obj of where we are coming from and going to
+var endCord = {x: 200, y: 190};
+
+var highRankCords = [];                /*Init a stack of the highest rank cords that we will know about.
+                                       / We haven't found any yet so this is empty*/
+
+var lowRankCords = [];                     //Create a stack for the highest 
+lowRankCords.push({xcord: startCord.x, ycord: startCord.y});  
+                                       /*Currently the only cord we know the rank of is the 0th one at the
+                                        start so push that */ 
+
+var board = new Array(xlen);           //Create an array to act as our board
+
+for (var i = 0; i < board.length; i++) {
+    /* INIT the array with the correct demensions and a large value in every square.
+    This ensures that it is always bigger than the route back to the start and so 
+    will never go back the wrong way */
+    board[i] = new Array(ylen);
+    for (var j = 0; j < board[i].length; j++) {
+        board[i][j] = xlen*ylen;
+    }
+}
+
+board[startCord.x][startCord.y] = 0;   //Start
+board[endCord.x][endCord.y] = 'X';     //End x marks the spot
+
+console.log(board); //Show the user what we are starting with
+
 function validToAdd(x,y,value) {
-    console.log('valid to add caled x: ', x, ' y: ', y, ' with value of ', value);
-    if (x >= 0 && y >= 0 && x <= 5-1 && y <= 5-1) {
-        console.log(board);
-        if (board[x][y]==8) {
-            console.log('Returning X for cords x: ', x, ' y: ', y, ' with value of ', value);
+/*
+    Checks weather it is valid to add a distance to the start to this cord
+    if it is then it adds it. If it sees the end it returns 'X'
+
+    x: int
+    y: int
+    value: int
+    returns: str
+*/
+    if (x >= 0 && y >= 0 && x <= xlen-1 && y <= ylen-1) {
+    //Is the cordinate not on the board
+        if (board[x][y]=='X') {
             return 'X'
         } else if (value < board[x][y]) {
+        // Is there a quicker way to get to that x value
             board[x][y] = value;
-            toBeCheckedCords.push({xcord:x, ycord:y}); 
+            highRankCords.push({xcord:x, ycord:y}); //Add it to the stack of high value numbers
         }
     }
 }
 
+//-----------------------------MAIN PROGRAM-----------------------------
 
-
-var xlen = 5;
-var ylen = 5;
-var board = new Array(xlen);
-
-for (var i = 0; i < board.length; i++) {
-    board[i] = new Array(ylen);
-    for (var j = 0; j < board[i].length; j++) {
-        board[i][j] = 9;
-    }
-}
-
-board[1][1] = 0;
-board[3][3] = 8
-
-console.log(board);
-
-checkedCords = [];
-checkedCords.push({xcord: 1, ycord: 1});
-toBeCheckedCords = [];
 found = false;
 
-
 while (!found) {
-    console.log('In while not found');
-    for (var i = 0; i < checkedCords.length; i++) {
-        console.log(checkedCords[i]);
-        startx = checkedCords[i].xcord;
-        starty = checkedCords[i].ycord;
-        console.log('Checking x: ', startx, ', y: ', starty);
+    for (var i = 0; i < lowRankCords.length; i++) {
+        //Go through all the low rank items and check for a higher rank around them
+        startx = lowRankCords[i].xcord;
+        starty = lowRankCords[i].ycord;
         if (
             validToAdd(startx + 1, starty, board[startx][starty] + 1) == 'X' ||
             validToAdd(startx, starty + 1, board[startx][starty] + 1) == 'X' ||
             validToAdd(startx - 1, starty, board[startx][starty] + 1) == 'X' ||
             validToAdd(startx, starty - 1, board[startx][starty] + 1) == 'X'  
-        ) {
-            console.log('WE GOT THERE!!!!');
+        ) 
+        //All one if statement, checks cords above, bellow, left and right
+        {
             found = true;
             break;
         }
     }
-    console.log('To be checked cords ', toBeCheckedCords);
-    if (toBeCheckedCords.length == 0) {
+
+    if (highRankCords.length == 0) {
         found = true;
-        console.log('NO SOLUTION FOUND')
+        console.log('ERROR: No Route found');
+        //If we ever run out of high rank cords then there is no possible route
     }
-    checkedCords = toBeCheckedCords;
-    toBeCheckedCords = [];
+
+    lowRankCords = highRankCords;   //Low becomes high before we loop again.
+    highRankCords = [];
 }
+
+console.log(board); //Show the user the end result
+
