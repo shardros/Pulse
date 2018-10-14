694be92ffbaeb5f654182d8a29f332719ef5d4c8
shardros <shardro.s@gmail.com>
2018-10-14 15:12:18 +0100

    Wave propogation to find a point from another

--- Project files/app.js
+++ Project files/app.js
@@ -1,21 +1,6 @@
 xDemension = 10
 yDemension = 10
 
-class PCB {
-    constructor (x, y) {
-        this.xlen = x;
-        this.ylen = y;
-        this.nets = [];
-        var board = new Array(this.xlen);
-
-        for (var i = 0; i < board.length; i++) {
-            board[i] = new Array(this.ylen);
-        }
-    }
-
-
-}
-
 class Net {
     constructor (x1, y1, x2, y2) {
         this.x1 = x1;
@@ -23,9 +8,60 @@ class Net {
         this.y1 = y1;
         this.y2 = y2;
     }
+}
 
-    route (pcb) {
+class PCB {
+     constructor (x, y) {
+        var xlen = x;
+        var ylen = y;
+        this.nets = [];
+        this.board = new Array(xlen);
+
+        for (var i = 0; i < this.board.length; i++) {
+            this.board[i] = new Array(ylen);
+            for (var j = 0; j < this.board[i].length; j++) {
+                this.board[i][j] = x*y;
+            }
+        }
+    }
+
+    addNet (net) {
+        nets.push(net);
+        this.board[net.x1][net.y1] = 0;
+        this.board[net.x2][net.y2] = 'X'; 
+    }
+}
+
+class router {
+    constructor (Pcb) {
+        this.board = Pcb.board;
+    }
+
+    route () {
+        
+    }
+
+    waveAround (cellx,celly) {
+        distFromStart = 0;
+        var distFromStart = this.board[cellx][celly] + 1;
+        console.log('Called WaveAround: ', distFromStart);
+        for (var x = cellx-1; x < cellx + 2; x++) {
+            for (var y = celly-1; y < celly + 2; y++) {
                 
+                if ((x >= 0) && (y >= 0)) {
+                    if (this.board[x][y] = 'X') {
+                        console.log('We Got there guys!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+                    }
+                    if (this.board[x][y] > distFromStart) {
+
+                        this.board[x][y] = distFromStart + 1;
+                        console.log(this.board);
+                    }     
+                } 
+            }        
+        }   
+
+
     }
 }
 
@@ -35,3 +71,6 @@ pcb.nets[0] = new Net(1,1,9,9);
 
 console.log(pcb.nets);
 
+router = new router(pcb);
+
+router.waveAround(1,1);
