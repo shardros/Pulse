7586826caee7751be876dcdf66e5b4b7f4ffd5e3
Edwin Shepherd <shardro.s@gmail.com>
2018-10-19 11:16:54 +0100

    Add files via upload

--- Project files/app.js
+++ Project files/app.js
@@ -1,3 +1,11 @@
+/*
+H   H EEEEE L     L      OOO       W   W  OOO  RRRR  L     DDDD  !!
+H   H E     L     L     O   O      W W W O   O R   R L     D   D !! 
+HHHHH EEEEE L     L     O   O      W W W O   O RRRR  L     D   D !! 
+H   H E     L     L     O   O  ,,   W W  O   O R   R L     D   D    
+H   H EEEEE LLLLL LLLLL  OOO  ,,    W W   OOO  R   R LLLLL DDDD  !!
+*/
+
 class Net {
     constructor (x1,y1,x2,y2) {
         this.x1 = x1;
@@ -9,14 +17,14 @@ class Net {
 
 class Board {
     constructor(XLen, YLen, netList) {
-        this.xLen = XLen;
-        this.yLen = YLen;
+        let xLen = XLen;
+        let yLen = YLen;
 
         this.netList = netList;
         this.boardArray = new Array(xLen);            //Create an array to act as our board
 
         for (var i = 0; i < this.boardArray.length; i++) {
-            this.boardArray[i] = new Array(this.yLen);
+            this.boardArray[i] = new Array(yLen);
         }
     }
 }
@@ -29,9 +37,10 @@ Object.defineProperty(Board, "prop", {    //NOTE TO FUTURE ME: Work out how to i
 class NetRouter {
     constructor (board) {
         /*This constructor needs to not over write and of the stuff already on the baord array*/ 
-        this.b = board; //******NOTE TO SELF****** Should this be a private property
+        this.b = board; //******NOTE TO SELF****** Should this be a private property, yes if you can make it do that. using var gives it scope to the constructor
+                       //Using this is dirty.
 
-        var maxDist = b.XLen * b.YLen; 
+        var maxDist = this.b.XLen * this.b.YLen; 
         //This is the worst possible and longest route that we could take on the board so set the default values of everything to that
 
         for (var i = 0; i < this.b.boardArray.length; i++) {
@@ -44,16 +53,24 @@ class NetRouter {
 
 
     routeNet (net) {
-        let highDistCords, lowDistCords = []
+        /*
+        ToDo:
+            - There maybe ways to refactor this to use arrow functions
+        */
+       let highDistCords, lowDistCords = []
 
+        lowDistCords.push({x1: net.x1, y1: net.y1});
+
+  
         let validToAdd = function (x,y,value) {  //Declare a function with limited scope. I.E. local to the routeNet function 
-            if (x >= 0 && y >= 0 && x <= xlen-1 && y <= ylen-1) { //Is the cordinate not on the board
+            console.log(this);
+            if (x >= 0 && y >= 0 && x <= this.b.XLen-1 && y <= this.b.YLen-1 && this.b.boardArray[x][y] != '#') { //Is the cordinate not on the board & and can we draw a wire here?
                 if (this.b.boardArray[x][y]=='X') {            //Is it the end point that we are looking for?
                     return 'X'
-                } else if (value < this.board[x][y]) {
+                } else if (value < b[x][y]) {
                 // Is there a quicker way to get to that x value
-                    this.board[x][y] = value;
-                    highDistCords.push({xcord:x, ycord:y}); //Add it to the stack of high value numbers
+                    this.b.boardArray[x][y] = value;
+                    highDistCords.push({xcord:x, ycord:y}); //Add it to the stack of high dist cords
                 }
             }
             
@@ -66,18 +83,18 @@ class NetRouter {
         while (!found) {
             for (var i = 0; i < lowDistCords.length; i++) {
                 //Go through all the low rank cords and check for a higher rank around them
-                startx = lowDistCords[i].xcord;
-                starty = lowDistCords[i].ycord;
+                let x1 = lowDistCords[i].x1;
+                let y1 = lowDistCords[i].y1;
                 //Find the x and y of the currently selected low rank cord
         
-                neighbours.push(validToAdd(startx + 1, starty, board[startx][starty] + 1));
-                neighbours.push(validToAdd(startx, starty + 1, board[startx][starty] + 1));
-                neighbours.push(validToAdd(startx - 1, starty, board[startx][starty] + 1));
-                neighbours.push(validToAdd(startx, starty - 1, board[startx][starty] + 1));
+                let NewValue = this.b.boardArray[x1][y1] + 1; 
+
+                neighbours.push(validToAdd(x1 + 1, y1, NewValue));
+                neighbours.push(validToAdd(x1, y1 + 1, NewValue));
+                neighbours.push(validToAdd(x1 - 1, y1, NewValue));
+                neighbours.push(validToAdd(x1, y1 - 1, NewValue));
                 //checks cords above, bellow, left and right to see if we can lower their rank or if they are the finish
                 if (neighbours.includes('X')) { 
-                /*Note it is important that the function calls are not put in the if statement otherwise lazy evaluation
-                 *May cause some errors */
                     found = true;
                     console.log('FOUND X');
                     break;
@@ -112,6 +129,8 @@ class BoardRouter extends NetRouter {  //Subclass the board class
 let ANet = new Net(1,1,3,3);
 let ANetList = [ANet];
 
-let r = new NetRouter(10,10,ANetList);
+let B = new Board(10,10,ANetList);
 
-console.log(r.pcb);
+let BR = new BoardRouter(B);
+
+BR.routeNet(ANet);
